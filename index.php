5c8877d998b3f0a2043ec10ae93fda8cbd67e5ca
Gauthier Baras <101516806+Gauthier5021@users.noreply.github.com>
2022-06-28 11:16:27 +0200

    Comparateur de prix

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+    <html lang="fr">
+
+      <head>
+        <meta name="author" content="Gauthier BARAS" />
+        <meta charset="utf-8" />
+        <link href="index.css" rel="stylesheet" />
+        <title>Autosun</title>
+      </head>
+
+      <body>
+
+        <!-- Header of the page -->
+            <?php include("nav.php") ?>
+
+        <!-- Best Known List -->
+            <div class="List">
+               <h3 class="CategoryTitle">CATEGORIES</h3><br />
+                <ul class="NavPhotovoltaique">
+                  <li>SunPower</li>
+                  <li>Enphase Onduleur</li>
+                  <li>Fixation Esdec</li>
+                </ul>
+                <ul class="NavDomotique">
+                  <li>Raspberry PI4 4GB</li>
+                  <li>Ecran HMTECH</li>
+                  <li>Pince Aeotec</li>
+                </ul>
+                <ul class="NavElectrique">
+                  <li>Cable Electrique</li>
+                  <li>Disjoncteur</li>
+                  <li>Tableau Electrique</li>
+                </ul>
+                <ul class="NavPlomberie">
+                  <li>Vannes</li>
+                  <li>Raccord Laiton</li>
+                  <li>Raccord Multicouche</li>
+                </ul>
+                <ul class="NavClim">
+                  <li>Goulotte</li>
+                  <li>Accessoire Clim</li>
+                  <li>Liaison Frigo</li>
+                </ul>
+            </div>
+
+      </body>
+    
+    </html>
---
+++ nav.php
@@ -0,0 +1,19 @@
+<head>
+  <meta name="author" content="Gauthier BARAS" />
+  <meta charset="utf-8" />
+  <link href="index.css" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="Header">
+    <h1 class="MainTitle">Liste Autosun</h1>
+      <ul class="NavBar">
+        <li class="Menu">Acceuil</li>
+        <li>Photovoltaique</li>
+        <li>Domotique</li>
+        <li>Electrique</li>
+        <li>Plomberie</li>
+        <li>Climatisation</li>
+      </ul>
+    </div>
+</body>   
