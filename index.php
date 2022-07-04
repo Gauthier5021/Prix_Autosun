d4dca702cd44bbcaefac02e2974096d30b535ceb
Gauthier Baras <101516806+Gauthier5021@users.noreply.github.com>
2022-07-04 18:33:00 +0200

    Mise à joue serveur debian 11

--- index.php
+++ index.php
@@ -12,35 +12,37 @@
       <body>
 
         <!-- Header of the page -->
-            <?php include("nav.php") ?>
+            <?php include("nav.php"); ?>
+
+            <?php include("fonction_electrique.php"); ?>
+
+            <?php echo ConnectionMysql(); ?>
 
         <!-- Best Known List -->
             <div class="List">
                <h3 class="CategoryTitle">CATEGORIES</h3><br />
-                <ul class="NavPhotovoltaique">
-                  <li>SunPower</li>
-                  <li>Enphase Onduleur</li>
-                  <li>Fixation Esdec</li>
-                </ul>
                 <ul class="NavDomotique">
                   <li>Raspberry PI4 4GB</li>
                   <li>Ecran HMTECH</li>
                   <li>Pince Aeotec</li>
                 </ul>
                 <ul class="NavElectrique">
-                  <li>Cable Electrique</li>
-                  <li>Disjoncteur</li>
-                  <li>Tableau Electrique</li>
+                  <li><a href="<?php echo FournisseurOption1(); ?>">Fournisseur 1</a></li>
+                  <li><a href="<?php echo FournisseurOption2(); ?>">Fournisseur 2</a></li>
+                  <li>Matèriel</li>
                 </ul>
                 <ul class="NavPlomberie">
-                  <li>Vannes</li>
-                  <li>Raccord Laiton</li>
-                  <li>Raccord Multicouche</li>
+                  <li>Fournisseur 1</li>
+                  <li>Fournisseur 2</li>
                 </ul>
                 <ul class="NavClim">
-                  <li>Goulotte</li>
-                  <li>Accessoire Clim</li>
-                  <li>Liaison Frigo</li>
+                  <li>Fournisseur 1</li>
+                  <li>Fournisseur 2</li>
+                </ul>
+                <ul class="TauxInflation">
+                  <li>Cuivre</li>
+                  <li>Or</li>
+                  <li>Fer</li>
                 </ul>
             </div>
 
